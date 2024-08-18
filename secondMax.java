5ca2fada73762f92bdf87cbe31f2cdc764912d42
sarvesh bhapkar <sarveshbhapkar100@gmail.com>
2024-08-18 12:29:13 +0530

    secindmax

---
+++ secondMax.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class secondMax {
+    
+   
+    public static void main(String[] args) {
+        int [] arr= {12,23,34,3354,56,67,78,89};
+        
+        Arrays.sort(arr);
+        System.out.println(arr);
+
+        }
+}
